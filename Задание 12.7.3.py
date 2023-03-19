244b3769d16fdbe63a275d063dd8b4241848e474
AlexAVI1 <xopela@yandex.ru>
2023-03-19 20:37:53 +0300

    Initial commit

---
+++ Задание 12.7.3.py
@@ -0,0 +1,6 @@
+money = int(input('Введите сумму, которую планируете положить под процент на год '))
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+deposit = []
+for item in per_cent:
+    deposit.append(per_cent[item] * money/100)
+print('Максимальная сумма, которую вы можете заработать -', max(deposit))
